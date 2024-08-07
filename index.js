96f71fcb935ef76be5c5da41bd7a5577db125452
Iyobosa <omosiyobo@gmail.com>
2024-08-07 18:26:17 +0100

    ..

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Citizen Report Api Testing');
+    res.send('Welcome to Citizen Report Api');
 });
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
